3a93110c6007243b813aba83621b53f6fcc6c976
ZAHIR <zahirkaoutar2003@gmail.com>
2023-12-05 20:47:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let img=document.querySelector('.image');
+let container=document.querySelector('.sec');
+
+function  imageslider(option){
+   img.src=option;
+}
+function color(option){
+    container.style.background=option;
+
+}
